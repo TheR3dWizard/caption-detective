6c80d372686806d70ae60473bd407765fe05bb0a
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-12-15 15:35:23 +0530

    helper class with all necessary functions in one place

---
+++ helper.py
@@ -0,0 +1,98 @@
+import json
+import hashlib
+from itertools import groupby
+from collections import namedtuple
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+from subtitleparser import parse_subs
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+class Helper:
+    def __init__(self) -> None:
+        self.elasticpassword = os.getenv("ES_PASSWORD")
+        self.elasticsearchhandler = Elasticsearch(
+            [{'host': 'localhost', 'port': 9200, 'scheme': 'https'}],
+            basic_auth=('elastic', self.elasticpassword),
+            ca_certs='http_ca.crt'
+        )
+    def addmovietojson(self, key, value):
+        file_path = "hashreference.json"
+        try:
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            data = {}
+
+        data[key] = value
+
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+    def hash(self, value):
+        return hashlib.sha256(value.encode()).hexdigest()
+    
+    def parsesubtitle(self, fpath):
+        with open(fpath) as f:
+            res = [list(g) for b, g in groupby(f, lambda x: bool(x.strip())) if b]
+
+        Subtitle = namedtuple('Subtitle', 'sub_id start end text')
+
+        subs = []
+
+        # grouping
+        for sub in res:
+            if len(sub) >= 3:  # not strictly necessary, but better safe than sorry
+                sub = [x.strip() for x in sub]
+                sub_id, start_end, *content = sub  # py3 syntax
+                start, end = start_end.split(' --> ')
+
+                # ints only
+                sub_id = int(sub_id.lstrip('\ufeff'))
+
+                # join multi-line text
+                text = ', '.join(content)
+
+                subs.append(Subtitle(
+                    sub_id,
+                    start,
+                    end,
+                    text
+                ))
+
+        es_ready_subs = []
+
+        for index, sub_object in enumerate(subs):
+            prev_sub_text = ''
+            next_sub_text = ''
+
+            if index > 0:
+                prev_sub_text = subs[index - 1].text + ' '
+
+            if index < len(subs) - 1:
+                next_sub_text = ' ' + subs[index + 1].text
+
+            es_ready_subs.append(dict(
+                **sub_object._asdict(),
+                overlapping_text=prev_sub_text + sub_object.text + next_sub_text
+            ))
+
+        return es_ready_subs
+        
+    def elasticingest(self, filepath, indexname):
+        parsedsubtitle = self.parsesubtitle(filepath)
+        actions = [
+            {
+                "_index": indexname,
+                "_id": sub_group['sub_id'],
+                "_source": sub_group
+            } for sub_group in parsedsubtitle
+        ]
+        
+        try:
+            bulk(self.elasticsearchhandler, actions)
+            return 'success'
+        except Exception as e:
+            return str(e)
