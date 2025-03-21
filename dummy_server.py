fb3a9a4fa0c2634bfbb0606e474fe9d12ff64dc5
Sreeraghavan R <34707235+TheR3dWizard@users.noreply.github.com>
2025-03-21 08:51:44 +0530

    changed the dummy server

--- dummy_server.py
+++ dummy_server.py
@@ -1,10 +1,8 @@
-from helper import Helper
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-hclass = Helper()
 
 @app.route('/')
 def home():
